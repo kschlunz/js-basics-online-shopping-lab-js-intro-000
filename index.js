907f2e87afbd1aa26eaa2f121164635e1142b936
Kate Schlunz <schlunzk@gmail.com>
2018-01-30 15:49:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ function placeOrder(cardNumber) {
   return console.log("Sorry, we don't have a credit card on file for you.")
 
   if(cardNumber===cardNumber){
-    return console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+    return console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
 
   //return delete cart;
